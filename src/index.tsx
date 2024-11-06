0b2329dd5088f12961597339d8d09028f314ebc1
madliani <madliani@hotmail.com>
2024-11-06 19:54:49 +0400

    fix: fix index

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-const root = document.getElementById("root");
+try {
+  const root = document.getElementById("root");
 
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+  if (root) {
+    ReactDOM.createRoot(root).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  }
+} catch (error) {
+  console.error(error);
 }
